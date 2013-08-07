dda14047252a429c7291880b4c4b2667eaea3382
masaki muto <muto.masaki@unipro.co.jp>
2013-08-08 01:01:58 +0900

    Import scene manager from UALS

---
+++ MasaLib/Scene/SceneBase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Masa.Lib.Scene
+{
+	public abstract class SceneBase : IDisposable
+	{
+
+		static protected readonly int Width;
+		static protected readonly int Height;
+		readonly int InputWaitTime = 5;
+
+		protected int Count { get; private set; }
+		int InputWaitCount { get; set; }
+		public bool IsDestroyed { get; private set; }//SceneManagerにより破棄される待ちフラグ
+		public bool IsSuspended { get; private set; }//
+
+		
+
+		public SceneBase()
+		{
+
+		}
+
+		public virtual void Update(bool isActive)
+		{
+			Count++;
+			if (isActive)
+			{
+				InputWaitCount++;
+				Control();
+			}
+			else
+			{
+				InputWaitCount = 0;
+			}
+		}
+
+		/// <summary>
+		/// 決定ボタンを受け入れるか
+		/// </summary>
+		/// <returns></returns>
+		protected bool CanAcceptInput()
+		{
+			return InputWaitCount >= InputWaitTime;
+		}
+
+
+		public abstract void Control();
+
+
+		public abstract void Draw(bool isActive);
+
+		public virtual void Suspend()
+		{
+			Debug.Assert(!IsSuspended);
+			IsSuspended = true;
+		}
+
+		public virtual void Resume()
+		{
+			Debug.Assert(IsSuspended);
+			IsSuspended = false;
+		}
+
+		/// <summary>
+		/// 自身を閉じて下のものにActiveを譲る
+		/// </summary>
+		public virtual void Exit()
+		{
+			IsDestroyed = true;
+		}
+
+		public abstract void Dispose();
+	}
+}
---
+++ MasaLib/Scene/SceneManager.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Masa.Lib.Scene
+{
+	public class SceneManager : IDisposable
+	{
+		LinkedList<SceneBase> scenes;
+		Queue<SceneBase> addQueue, removeQueue;
+		bool needSuspend, needResume;
+
+		public SceneManager()
+		{
+			addQueue = new Queue<SceneBase>();
+			removeQueue = new Queue<SceneBase>();
+			scenes = new LinkedList<SceneBase>();
+
+		}
+
+		public void Update()
+		{
+			while (removeQueue.Any())
+			{
+				var rem = removeQueue.Dequeue();
+				scenes.Remove(rem);
+				rem.Dispose();
+			}
+			if (needResume)
+			{
+				ResumeAllInnner();
+			}
+			if (needSuspend)
+			{
+				SuspendAllInnner();
+			}
+			while (addQueue.Any())
+			{
+				scenes.AddFirst(addQueue.Dequeue());
+			}
+			var first = true;
+			foreach (var item in scenes)
+			{
+				if (!item.IsSuspended)
+				{
+					item.Update(first);
+					if (item.IsDestroyed)
+					{
+						removeQueue.Enqueue(item);
+					}
+				}
+				first = false;
+			}
+		}
+
+		/// <summary>
+		/// 下のレイヤーも描画する
+		/// </summary>
+		public void Draw()
+		{
+			foreach (var item in scenes.Skip(1).Reverse())
+			{
+				if (!item.IsSuspended)
+				{
+					item.Draw(false);
+				}
+			}
+			scenes.First().Draw(true);
+		}
+
+
+		
+
+		public void Add(SceneBase scene)
+		{
+			addQueue.Enqueue(scene);
+		}
+
+		public void SuspendAll()
+		{
+			needSuspend = true;
+		}
+
+		void SuspendAllInnner()
+		{
+			foreach (var item in scenes)
+			{
+				item.Suspend();
+			}
+			needSuspend = false;
+		}
+
+		public void ResumeAll()
+		{
+			needResume = true;
+		}
+
+		void ResumeAllInnner()
+		{
+			foreach (var item in scenes)
+			{
+				item.Resume();
+			}
+			needResume = false;
+		}
+
+		public void Dispose()
+		{
+			if (addQueue != null)
+			{
+				foreach (var item in addQueue)
+				{
+					item.Dispose();
+				}
+			}
+			if (scenes != null)
+			{
+				foreach (var item in scenes)
+				{
+					item.Dispose();
+				}
+			}
+			addQueue = null;
+			scenes = null;
+			removeQueue = null;
+			GC.SuppressFinalize(this);
+		}
+
+		~SceneManager()
+		{
+			Dispose();
+		}
+	}
+
+}
